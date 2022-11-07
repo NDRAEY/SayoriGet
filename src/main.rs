1dc7dd282c2a671693ab18cab779a495c2e529ff
NDRAEY <andrejpavlenko666@gmail.com>
2022-11-08 08:21:19 +0900

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,95 @@
 mod gh_api;
 use crate::gh_api::gh_api::GitHubApi;
 use std::io;
-use std::process;
 use std::io::Write;
+use std::process;
 
 fn log(message: String) {
-	println!("[LOG] {}", message);
+    println!("[LOG] {}", message);
 }
 
 fn warn(message: String) {
-	println!("[\x1b[33;1mWARN\x1b[0m] {}", message);
+    println!("[\x1b[33;1mWARN\x1b[0m] {}", message);
 }
 
 fn error(message: String) {
-	println!("[\x1b[31;1mERR\x1b[0m] {}", message);
+    println!("[\x1b[31;1mERR\x1b[0m] {}", message);
 }
 
 fn main() {
-	println!("SayoriGet v1.0 by NDRAEY 2022");
+    println!("SayoriGet v1.0 by NDRAEY 2022");
 
-	let sayori_original = GitHubApi {
-		owner: "pimnik98".to_string(),
-		repo: "SayoriOS".to_string()
-	};
+    let sayori_original = GitHubApi {
+        owner: "pimnik98".to_string(),
+        repo: "SayoriOS".to_string(),
+    };
 
-	let mut version: Option<String> = None;
+    let mut version: Option<String> = None;
 
-	let data = sayori_original.method("releases".to_string());
+    let data = sayori_original.method("releases".to_string());
+    match data {
+        Ok(values) => {
+            println!("\n{}", "=".repeat(40));
 
-	match data {
-		Ok(values) => {
-			println!("\n{}", "=".repeat(40));
-			
-			let arraylen = values.as_array().unwrap().len();
-			let mut versions: Vec<&str> = Vec::new();
-			
-			for i in 0..arraylen {
-				let curobj = values[i].as_object().unwrap();
+            let arraylen = values.as_array().unwrap().len();
+            let mut versions: Vec<&str> = Vec::new();
 
-				let tag_name = &curobj["tag_name"].as_str().unwrap();
-				let name = &curobj["name"].as_str().unwrap();
+            for i in 0..arraylen {
+                let curobj = values[i].as_object().unwrap();
 
-				println!("[{}] {} ({})", i+1, name, tag_name);
-				versions.push(tag_name);
-			}
+                let tag_name = &curobj["tag_name"].as_str().unwrap();
+                let name = &curobj["name"].as_str().unwrap();
 
-			let mut selected = arraylen+1;
+                println!("[{}] {} ({})", i + 1, name, tag_name);
+                versions.push(tag_name);
+            }
 
-			if version == None {  // For future
-				println!("");
-				while (selected < 0) || (selected > arraylen) {
-					let mut tempstr = String::new();
-					print!("Select entry > ");
-					io::stdout().flush().unwrap();
-					io::stdin().read_line(&mut tempstr).expect("ERR: Unable to read user input");
+            let mut selected = arraylen + 1;
 
-					tempstr = tempstr.trim().to_string();
+            if version == None {
+                // For future
+                println!("");
+                while selected > arraylen {
+                    let mut tempstr = String::new();
+                    print!("Select entry > ");
+                    io::stdout().flush().unwrap();
+                    io::stdin()
+                        .read_line(&mut tempstr)
+                        .expect("ERR: Unable to read user input");
 
-					version = Some(tempstr);
+                    tempstr = tempstr.trim().to_string();
+                    version = Some(tempstr);
+                    selected = version.unwrap().parse::<usize>().unwrap() - 1;
+                }
+            }
 
-					selected = version.unwrap().parse::<usize>().unwrap() - 1;
-				}
-			}
-			
-			println!("Selected version is: {}", versions[selected]);
+            log(format!("Selected version is: {}", versions[selected]));
 
-			let mut url: Option<&str> = None;
+            let mut url: Option<&str> = None;
 
-			let curobj = values[selected]["assets"].as_array().unwrap();
-			let mut found = false;
-			
-			for j in 0..curobj.len() {
-				if curobj[j]["content_type"] == "application/x-cd-image" {
-					url = Some(curobj[j]["browser_download_url"].as_str().unwrap());
-					found = true;
-					break;
-				}
-			}
+            let curobj = values[selected]["assets"].as_array().unwrap();
+            //let mut found = false;
 
-			if url == None {
-				error(format!("No ISO images found for version: {}", versions[selected]));
-				process::exit(1);
-			}
+            for j in 0..curobj.len() {
+                if curobj[j]["content_type"] == "application/x-cd-image" {
+                    url = Some(curobj[j]["browser_download_url"].as_str().unwrap());
+                    //found = true;
+                    break;
+                }
+            }
 
-			println!("This url: {}", url.unwrap());
-		},
-		Err(err) => {
-			error(format!("Failed to parse JSON! ({})", err));
-		}
-	}
+            if url == None {
+                error(format!(
+                    "No ISO images found for version: {}",
+                    versions[selected]
+                ));
+                process::exit(1);
+            }
+
+            println!("This url: {}", url.unwrap());
+        }
+        Err(err) => {
+            error(format!("Failed to parse JSON! ({})", err));
+        }
+    }
 }
